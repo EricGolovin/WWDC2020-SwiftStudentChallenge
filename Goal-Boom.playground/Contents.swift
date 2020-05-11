dc272b11c2cc0d0836aa5b74b079e3dcb8a297c9
EricGolovin <ericgolovin@outlook.com>
2020-05-12 00:27:00 +0300

    Add First Screen.

--- Goal-Boom.playground/Contents.swift
+++ Goal-Boom.playground/Contents.swift
@@ -1,3 +1,4 @@
+import Foundation
 import UIKit
 import CoreGraphics
 import PlaygroundSupport
@@ -18,37 +19,71 @@ class IntroductionViewController: UIViewController {
     }
     
     func updateUI() {
-        let button = UIButton(type: .custom)
+        let button = UIButton()
         
-        button.center = view.center
-        button.backgroundColor = .blue
-        button.setTitle("Press Me!", for: .normal)
-        button.sizeToFit()
-        //        button.center.x = view.bounds.width / 2
-        //        button.center.y = view.frame.height / 2
-        
-        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-        
-        let animationView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        let animationView = UIView(frame: view.bounds)
         let blurAffect = UIBlurEffect(style: .systemMaterialLight)
         let blurView = UIVisualEffectView(effect: blurAffect)
+        
+        //        let frameX = animationView.frame.maxX / 10
+        //        let frameY = animationView.frame.maxY / 10
+        let informationView = UIView() // frame: CGRect(x: frameX, y: frameY, width:CGFloat(animationView.frame.maxX - frameX * 2), height: animationView.frame.height / 2)
+        
+        //        button.center.x = view.frame.width/2
+        //        button.center.y = view.frame.height/2
+        button.backgroundColor = .white
+        button.setTitle("Press Me!", for: .normal)
+//        button.setAttributedTitle(NSAttributedString(string: "Press Me!", attributes: [NSAttributedStringKey: ]), for: <#T##UIControl.State#>)
+//        button.sizeToFit()
+        button.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        
+        informationView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+        informationView.layer.cornerRadius = 25
+        
         blurView.frame = animationView.bounds
         
+        let colors = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.yellow.cgColor, UIColor.cyan.cgColor]
+        animationView.layer.animate(colors: colors, duration: 5, sizing: animationView.bounds)
+        animationView.addSubview(blurView)
         
         
         self.view.addSubview(animationView)
-        animationView.addSubview(blurView)
-        self.view.addSubview(button)
+        informationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.addSubview(informationView)
         
-        UIView.animate(withDuration: 2, delay: 0, options: [.repeat, .autoreverse], animations: {
-            animationView.backgroundColor = .red
-            animationView.backgroundColor = .blue
-            animationView.backgroundColor = .green
-            animationView.backgroundColor = .yellow
-        }, completion: nil)
-//        UIView.animate(withDuration: 1) {
-//            visualEffectView.effect = UIBlurEffect(style: .dark)
-//        }
+        informationView.addSubview(button)
+        informationView.clipsToBounds = false
+        
+        let backButton = UIButton(type: .custom)
+        backButton.setTitle("<-", for: .normal)
+        backButton.backgroundColor = .white
+        
+        let stackView = UIStackView(arrangedSubviews: [backButton, button])
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.addSubview(stackView)
+        
+        stackView.arrangedSubviews.map { button in
+            button.layer.cornerRadius = 10
+//            button.setAtt
+        }
+        
+        
+        let constraits = [
+            informationView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40),
+            informationView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -40),
+            informationView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -300),
+            informationView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -40),
+                         stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -40),
+            stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 40),
+            stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 40),
+
+        ]
+        
+        NSLayoutConstraint.activate(constraits)
         
     }
     
@@ -56,7 +91,7 @@ class IntroductionViewController: UIViewController {
         print("Button Pressed")
         
         let configuration = ConfigurationViewController()
-//        navigationController?.pushViewController(configuration, animated: true)
+        //        navigationController?.pushViewController(configuration, animated: true)
         PlaygroundPage.current.liveView = configuration
     }
     
@@ -91,3 +126,32 @@ class ConfigurationViewController: UIViewController {
 let master = IntroductionViewController()
 let navigationContoller = UINavigationController(rootViewController: master)
 PlaygroundPage.current.liveView = master
+
+
+extension CALayer {
+    func animate(colors: [CGColor]?, duration: Double, sizing: CGRect) {
+        let gradient = CAGradientLayer()
+        gradient.frame = sizing
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradient.colors = [UIColor.red.cgColor, UIColor.green.cgColor]
+        gradient.locations =  [-0.5, 1.5]
+        
+        let animation = CABasicAnimation(keyPath: "colors")
+        if let colors = colors, colors.count > 4 {
+            animation.fromValue = [colors[0], colors[1]]
+            animation.toValue = [colors[2], colors[3]]
+            
+        } else {
+            animation.fromValue = [UIColor.red.cgColor, UIColor.blue.cgColor]
+            animation.toValue = [UIColor.yellow.cgColor, UIColor.cyan.cgColor]
+        }
+        animation.duration = 5.0
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        gradient.add(animation, forKey: nil)
+        
+        self.addSublayer(gradient)
+    }
+}
