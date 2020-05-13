0194e1c234ab4c99eaa8e33bd25352680952d6e9
EricGolovin <ericgolovin@outlook.com>
2020-05-13 23:23:39 +0300

    Add TableView Styling

--- Goal-Boom.playground/Contents.swift
+++ Goal-Boom.playground/Contents.swift
@@ -289,22 +289,53 @@ class CountryTableViewController: UIViewController, UITableViewDataSource, UITab
         regionManager.fetch()
         navigationController?.setNavigationBarHidden(true, animated: true)
         let gradientBackgroundView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-        gradientBackgroundView.layer.animate(colors: [lightBlueColor, UIColor.blue.cgColor, lightBlueColor, darkBlueColor], duration: 3.0, sizing: self.view.bounds)
+        gradientBackgroundView.layer.animate(colors: [lightBlueColor, UIColor.blue.cgColor, lightBlueColor, darkBlueColor], duration: 2.0, sizing: self.view.bounds)
         self.tableView = UITableView()
         self.tableView?.dataSource = self
         self.tableView?.delegate = self
         self.tableView?.backgroundColor = .white
-        self.tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "countryCell")
+        self.tableView?.register(BlurTableViewCell.self, forCellReuseIdentifier: "countryCell")
+        
+        let topView = UIView()
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        
+        let questionLabel = UILabel()
+        questionLabel.translatesAutoresizingMaskIntoConstraints = false
+        questionLabel.text = "Choose your \ncountry"
+        questionLabel.textColor = .white
+        questionLabel.numberOfLines = 0
+        questionLabel.font = UIFont(name: "Kefa", size: 40)
+        questionLabel.adjustsFontSizeToFitWidth = true
+        questionLabel.backgroundColor = .clear
+        
+        questionLabel.layer.shadowColor = UIColor.black.cgColor
+        questionLabel.layer.shadowRadius = 10
+        questionLabel.layer.shadowOffset = .zero
+        questionLabel.layer.shadowOpacity = 0.3
+        
+                
         view.addSubview(gradientBackgroundView)
+        topView.addSubview(questionLabel)
+        view.addSubview(topView)
         view.addSubview(self.tableView!)
         
         if let tableView = self.tableView {
             tableView.translatesAutoresizingMaskIntoConstraints = false
+            tableView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)
             let constraits = [
-                tableView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
+                topView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+                topView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
+                topView.heightAnchor.constraint(equalToConstant: 175),
+                topView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
+                tableView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 0),
                 tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
                 tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0),
                 tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
+                questionLabel.topAnchor.constraint(equalTo: topView.topAnchor, constant: 40),
+//                questionLabel.rightAnchor.constraint(equalTo: topView.rightAnchor, constant: 0),
+                questionLabel.bottomAnchor.constraint(equalTo: topView.bottomAnchor, constant: -5),
+                questionLabel.leftAnchor.constraint(equalTo: topView.leftAnchor, constant: 20),
             ]
             
             NSLayoutConstraint.activate(constraits)
@@ -321,22 +352,45 @@ class CountryTableViewController: UIViewController, UITableViewDataSource, UITab
         return regionManager.regionList[section].countries.count
     }
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return regionManager.regionList[section].regionName
-//    }
-    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let label =
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+        
+        let label = UILabel()
+        label.text = regionManager.regionList[section].regionName
+        label.textColor =  UIColor(red: 0.496, green: 0.496, blue: 0.496, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        headerView.addSubview(label)
+
+        let constaits = [
+            label.leftAnchor.constraint(equalTo: headerView.leftAnchor, constant: 15),
+            label.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+        ]
+
+        NSLayoutConstraint.activate(constaits)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 35
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath) as! BlurTableViewCell
         let region = regionManager.regionList[indexPath.section]
         let countryName = region.countries[indexPath.row]
         let countryFlag = region.flags[indexPath.row]
-        cell.textLabel?.text = "\(countryName) \(countryFlag)"
+        cell.set(emoji: countryFlag, country: countryName)
+        cell.accessoryType = .disclosureIndicator
+        cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.2)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -345,25 +399,61 @@ class CountryTableViewController: UIViewController, UITableViewDataSource, UITab
     }
 }
 
-//class CountryCell: UITableViewCell {
-//
-//    private let
-//
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//
-//
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
+class BlurTableViewCell: UITableViewCell {
+    
+    private var emojiLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont(name: "Helvetica", size: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowRadius = 7
+        label.layer.shadowOffset = .zero
+        label.layer.shadowOpacity = 0.3
+        
+        return label
+    }()
+    
+    private var infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Helvetica", size: 17)
+        label.textColor = UIColor(red: 0.496, green: 0.496, blue: 0.496, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(emojiLabel)
+        contentView.addSubview(infoLabel)
+
+        
+        let constaits = [
+            emojiLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            emojiLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            emojiLabel.widthAnchor.constraint(equalToConstant: 50),
+            infoLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            infoLabel.leftAnchor.constraint(equalTo: emojiLabel.leftAnchor, constant: 60),
+        ]
+        
+        NSLayoutConstraint.activate(constaits)
+    }
+    
+    func set(emoji: String, country: String) {
+        emojiLabel.text = emoji
+        infoLabel.text = country
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 let master = IntroductionViewController()
 let tableViewContoller = CountryTableViewController()
 let navigationContoller = UINavigationController(rootViewController: master)
-PlaygroundPage.current.liveView = tableViewContoller
+PlaygroundPage.current.liveView = navigationContoller
 
 
 final class ImageManager {
@@ -416,10 +506,6 @@ final class ImageManager {
         }
         return nil
     }
-}
-
-class BlurCell: UITableViewCell {
-    
 }
 
 extension CALayer {
